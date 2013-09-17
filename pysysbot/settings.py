5befa5c40e322d2096c2d127b23716b630fdf562
Fabian Affolter <fabian@affolter-engineering.ch>
2013-09-17 10:21:44 +0200

    Switch to BSD

--- pysysbot/settings.py
+++ pysysbot/settings.py
@@ -1,20 +1,7 @@
 # pysysbot - A simple python jabber bot for getting system information
 # Copyright (c) 2009-2013 Fabian Affolter <fabian@affolter-engineering.ch>
 #
-# All rights reserved.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Released under the BSD license. See COPYING file for details.
 #
 import os
 import sys
