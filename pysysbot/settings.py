02ecd3582fab275494cbe69b0a3f0de393d0f51f
Fabian Affolter <fabian@affolter-engineering.ch>
2015-11-13 17:09:45 +0100

    Update docstrings

--- pysysbot/settings.py
+++ pysysbot/settings.py
@@ -8,23 +8,21 @@ import sys
 import ConfigParser
 
 def read_config(file, config_jabber={}):
-    """Read the configuration data."""
+    """ Read the configuration data. """
     config_data = []
     if not(os.path.exists(file)):
-        print 'pysysbot configuration file does not exist.'
+        print('pysysbot configuration file does not exist.')
         sys.exit(0)
     else:
         configparse = ConfigParser.ConfigParser()
         configparse.read(file)
         config_jabber['username'] = configparse.get('pysysbot', 'username')
         if config_jabber['username'] == 'NOT_SET':
-            print 'Please edit your configuration file (%s) and set your JID' \
-                % (file)
+            print('Please edit your configuration file (%s) and set your JID', file)
             sys.exit(0)
         config_jabber['password'] = configparse.get('pysysbot', 'password')
         if config_jabber['password'] == 'NOT_SET':
-            print 'Please edit your configuration file (%s) and set your Jabber password' \
-                % (file)
+            print('Please edit your configuration file (%s) and set your Jabber password', file)
             sys.exit(0)
         config_data.append(config_jabber)
     return config_data
