5809ba0f7e915295a2ea119f1c941287804d6251
Fabian Affolter <fabian@affolter-engineering.ch>
2019-05-08 11:37:33 +0200

    Fix file header

--- pysysbot/__init__.py
+++ pysysbot/__init__.py
@@ -1,7 +1,1 @@
-# This file is part of pysysbot.
-#
-# pysysbot - A simple python jabber bot for getting system information
-# Copyright (c) 2009-2015 Fabian Affolter <fabian at affolter-engineering.ch>
-#
-# Released under the BSD license. See COPYING file for details.
-#
+"""Init file for pysysbot."""
