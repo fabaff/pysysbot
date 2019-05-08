5822f1ed45f7135d2078152245e9f2feb05ed39e
Fabian Affolter <fabian@affolter-engineering.ch>
2019-05-08 11:29:27 +0200

    Update style and imports

--- pysysbot/settings.py
+++ pysysbot/settings.py
@@ -1,28 +1,31 @@
-# pysysbot - A simple python jabber bot for getting system information
-# Copyright (c) 2009-2015 Fabian Affolter <fabian@affolter-engineering.ch>
-#
-# Released under the BSD license. See COPYING file for details.
-#
+"""Handler for configuration settings."""
 import os
 import sys
-import ConfigParser
+import configparser
+
 
 def read_config(file, config_jabber={}):
     """ Read the configuration data. """
     config_data = []
-    if not(os.path.exists(file)):
+    if not (os.path.exists(file)):
         print('pysysbot configuration file does not exist.')
         sys.exit(0)
     else:
-        configparse = ConfigParser.ConfigParser()
+        configparse = configparser.ConfigParser()
         configparse.read(file)
         config_jabber['username'] = configparse.get('pysysbot', 'username')
         if config_jabber['username'] == 'NOT_SET':
-            print('Please edit your configuration file (%s) and set your JID', file)
+            print(
+                'Please edit your configuration file (%s) and set your JID',
+                file,
+            )
             sys.exit(0)
         config_jabber['password'] = configparse.get('pysysbot', 'password')
         if config_jabber['password'] == 'NOT_SET':
-            print('Please edit your configuration file (%s) and set your Jabber password', file)
+            print(
+                'Please edit your configuration file (%s) and set your '
+                'Jabber password', file,
+            )
             sys.exit(0)
         config_data.append(config_jabber)
     return config_data
