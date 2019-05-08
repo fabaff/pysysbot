113e9cf12f3f0a5fb75d92670e84074148b6fa36
Fabian Affolter <fabian@affolter-engineering.ch>
2019-05-08 11:39:07 +0200

    Prepare for 0.3.0

--- setup.py
+++ setup.py
@@ -1,49 +1,53 @@
-# This file is part of pysysbot.
-#
-# pysysbot - A simple python jabber bot for getting system information
-# Copyright (c) 2009-2015 Fabian Affolter <fabian at affolter-engineering.ch>
-#
-# Released under the BSD license. See COPYING file for details.
-#
+#!/usr/bin/env python3
+"""Set up the pySysBot."""
+import os
+import sys
+
 from setuptools import setup
 
-if __name__ == '__main__':
-    setup(
-        name = 'pysysbot',
-        version="0.2.0",
-        description = 'Python based system jabber bot',
-        long_description = """This python jabber (XMPP) bot is based on \
-            the jabberbot framework (http://thpinfo.com/2007/python-jabberbot/).\
-            The bot is capable to display details about the system it \
-            is running on. """,
-        author = 'Fabian Affolter',
-        author_email = 'fabian@affolter-engineering.ch',
-        maintainer = 'Fabian Affolter',
-        maintainer_email = 'fabian@affolter-engineering.ch',
-        url = 'http://affolter-engineering.ch/pysysbot/',
-        license = 'BSD',
-        platforms = 'Linux',
-        packages = ['pysysbot'],
-        entry_points = {
-            'console_scripts': ['pysysbot = pysysbot.pysysbot:main']
-        },
-        include_package_data = True,
-        install_requires=[
-            'psutil',
-            'jabberbot',
-        ],
-        keywords = ['Jabber','XMPP','System','python'],
-        classifiers = [
-                'Development Status :: 4 - Beta',
-                'Environment :: Console',
-                'Intended Audience :: End Users/Desktop',
-                'Intended Audience :: System Administrators',
-                'License :: OSI Approved :: BSD License',
-                'Operating System :: POSIX',
-                'Programming Language :: Python',
-                'Topic :: Communications',
-                'Topic :: Internet',
-                'Topic :: System',
-                'Topic :: System :: Monitoring',
-                ],
-    )
+from pysysbot.constants import VERSION as __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as desc:
+    long_description = desc.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='pysysbot',
+    version=__version__,
+    description="Python based system jabber bot",
+    long_description=long_description,
+    author="Fabian Affolter",
+    author_email='fabian@affolter-engineering.ch',
+    maintainer="Fabian Affolter",
+    maintainer_email="fabian@affolter-engineering.ch",
+    url='http://affolter-engineering.ch/pysysbot/',
+    license='BSD',
+    platforms='Linux',
+    packages=['pysysbot'],
+    entry_points={
+        'console_scripts': ['pysysbot = pysysbot.pysysbot:main']
+    },
+    include_package_data=True,
+    install_requires=[
+        'psutil',
+        'slixmpp',
+    ],
+    keywords=['Jabber', 'XMPP', 'System', 'python'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Topic :: Communications',
+            'Topic :: Internet',
+            'Topic :: System',
+            'Topic :: System :: Monitoring',
+            ],
+)
